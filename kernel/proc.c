e9a9747e9a4228aae43b58753ecd02c3348af82d
thewall <1753762525@qq.com>
2024-05-27 18:28:17 +0800

    lab pgtbl  Speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,14 @@ found:
     return 0;
   }
 
+  //Allocate a usyscall page
+  if ((p->usc = (struct usyscall*)kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usc->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,7 +161,12 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
-  if(p->pagetable)
+
+  if (p->usc)
+    kfree((void*)p->usc);
+  p->usc = 0;
+
+  if (p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
   p->sz = 0;
@@ -196,6 +209,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  //映射USYSCALL在TRAPFRAME之下
+  if (mappages(pagetable, USYSCALL, PGSIZE,
+             (uint64)(p->usc), PTE_R | PTE_U) < 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +228,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
